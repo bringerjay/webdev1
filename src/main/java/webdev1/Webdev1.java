363e4869dbc4ed5c19d94d8f3215c22ff15b6b64
Zhou <jayzee@acbc32a0fc95.ant.amazon.com>
2019-05-15 20:51:36 -0700

    assignment1

---
+++ src/main/java/webdev1/Webdev1.java
@@ -0,0 +1,13 @@
+package webdev1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webdev1 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webdev1.class, args);
+    }
+
+};
