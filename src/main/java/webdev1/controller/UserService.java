be4f4424325446416607188d815b656603225062
Zhou <jayzee@acbc32a0fc95.ant.amazon.com>
2019-05-23 15:43:36 -0700

    assignment2

--- src/main/java/webdev1/controller/UserService.java
+++ src/main/java/webdev1/controller/UserService.java
@@ -1,25 +1,18 @@
 package webdev1.controller;
 import webdev1.model.*;
-
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.ListIterator;
-
 import javax.servlet.http.HttpSession;
-
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import jdk.javadoc.internal.doclets.formats.html.markup.HtmlAttr.Role;
-
 @RestController
 public class UserService{
 	static List<User> users = new ArrayList<User>();
